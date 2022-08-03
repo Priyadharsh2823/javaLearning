fe2b8529deb95eaef180a78122c8bab5b59ecbda
Priyadharsh2823 <109994291+Priyadharsh2823@users.noreply.github.com>
2022-08-03 12:25:23 -0400

    Create App.java

---
+++ Datatype/App.java
@@ -0,0 +1,32 @@
+package com.Datadriven.BankingApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 String name = "priya";
+	
+    public static void main( String[] args )
+    {
+    	
+    	
+    	int a = 10;
+    	//by default decimals val are double and whole values are int
+    	//float f = 297429.23947;
+    	float f = 347.347f;
+    	long l = 374203470237092l;
+    	float f1 = 34783247;
+    	double d2 =2983162;
+    	System.out.println(d2);
+    	System.out.println(f1);
+    	
+    	App obj = new App();
+    	
+    	
+    	System.out.println(obj.name);
+    	
+        System.out.println( "Hello World!" );
+    }
+}
